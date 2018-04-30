22a6716fda20534084b19e566e06f24288cff4cd
Kameron Brooks <kameron.cw.11@gmail.com>
2018-04-30 13:54:08 -0400

    Initial Commit

---
+++ CodeHeaderWriter/Editor/CodeHeaderWriter.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[ExecuteInEditMode]
+public class CodeHeaderWriter {
+
+    public static string DefaultHeader =
+@"
+// Copyright (c) [YEAR], [AUTHOR]
+// All rights reserved
+//
+//
+// THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+";
+    //Return a list of all cs files in the specified directory
+    public static string[] GetScriptNamesInDirectory(string directoryPath, bool includeSubdirectories = true)
+    {
+        // create new list of file paths
+        List<string> filePathList = new List<string>();
+        
+        if(Directory.Exists(directoryPath))
+        {
+            // iterate through files and add them to the list
+            string[] allFiles = Directory.GetFiles(directoryPath);
+            for(int i = 0; i < allFiles.Length; i += 1)
+            {
+                if(allFiles[i].EndsWith(".cs"))
+                {
+                    filePathList.Add(allFiles[i]);
+                }
+            }
+
+            // if include sub directories is true, recursively include all files from subdirectories
+            if(includeSubdirectories)
+            {
+                string[] subDirectories = Directory.GetDirectories(directoryPath);
+
+                for(int i = 0; i < subDirectories.Length; i += 1)
+                {
+                    string[] subDirectoryFiles = GetScriptNamesInDirectory(subDirectories[i]);
+                    if(subDirectoryFiles != null && subDirectoryFiles.Length > 0)
+                    {
+                        filePathList.AddRange(subDirectoryFiles);
+                    }
+                }
+            }
+        }
+
+        return filePathList.ToArray();    
+    }
+
+    public static void UpdateAllScriptsInDirectory(string header, string directoryPath, bool includeSubdirectories = true)
+    {
+        string[] filePaths = GetScriptNamesInDirectory(directoryPath, includeSubdirectories);
+        foreach (string filePath in filePaths)
+        {
+            UpdateFileHeader(filePath, header);
+        }
+    }
+
+    /// <summary>
+    /// Returns true if file was modified, and false if it was not
+    /// </summary>
+    /// <param name="filePath"></param>
+    /// <param name="header"></param>
+    /// <returns></returns>
+    public static bool UpdateFileHeader(string filePath, string header)
+    {
+        string fileContents = File.ReadAllText(filePath);
+
+        if(fileContents.Contains(header))
+        {
+            return false;
+        }
+
+        File.WriteAllText(filePath, header + "\n" + fileContents);
+        return true;
+        
+    }
+    
+    public static bool DoesFileHaveHeader(string filePath, string header)
+    {
+        string fileContents = File.ReadAllText(filePath);
+
+        return fileContents.Contains(header);
+        
+    }
+
+    
+}
---
+++ CodeHeaderWriter/Editor/CodeHeaderWriterEditorWindow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CodeHeaderWriterEditorWindow : EditorWindow {
+    private static CodeHeaderWriterEditorWindow _instance;
+    public static CodeHeaderWriterEditorWindow instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    [MenuItem("Open", menuItem = "Window/Code Headers")]
+    public static void Open()
+    {
+        _instance = GetWindow<CodeHeaderWriterEditorWindow>();
+  
+    }
+
+    private string _currentHeader = CodeHeaderWriter.DefaultHeader;
+    private bool _includeSubdirectories;
+
+    private void OnGUI()
+    {
+        _currentHeader = EditorGUILayout.TextArea(_currentHeader);
+        _includeSubdirectories = EditorGUILayout.Toggle("Include Subdirectories", _includeSubdirectories);
+        if(GUILayout.Button(new GUIContent("Add Headers", "Adds the specified header to all script files in a chosen directory that do not already contain it"))) {
+            AddHeaders();
+        }
+    }
+    private void AddHeaders()
+    {
+        string folderName = EditorUtility.OpenFolderPanel("Add Script Headers", Application.dataPath, "");
+        if(folderName != "")
+        {
+            CodeHeaderWriter.UpdateAllScriptsInDirectory(_currentHeader, folderName, _includeSubdirectories);
+        }
+    }
+    private void OnEnable()
+    {
+        string[] filenames = CodeHeaderWriter.GetScriptNamesInDirectory(Application.dataPath);
+
+        
+    }
+    private void OnDisable()
+    {
+        
+    }
+}
